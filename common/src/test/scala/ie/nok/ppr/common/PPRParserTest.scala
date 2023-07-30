af9675981865a4f3e764ca9e71a3e572729b838b
Sylwester Stocki <sylwester.stocki.rnd@gmail.com>
2023-07-30 20:23:34 +0100

    PPR Parser

---
+++ common/src/test/scala/ie/nok/ppr/common/PPRParserTest.scala
@@ -0,0 +1,27 @@
+package ie.nok.ppr.common
+
+import munit.FunSuite
+
+import java.io.File
+import scala.io.{BufferedSource, Source}
+
+class PPRParserTest extends FunSuite {
+
+  test("Parse PPR from February 2023") {
+    val path = getClass.getResource("/PPR-2023-02.csv")
+    val file: File = File(path.getPath)
+    val records = PPRParser.recordsFromCsv(file)
+    assertResult(records, 3838)
+  }
+
+  private def assertResult(records: List[PropertyPriceRegisterRecord], size: Int): Unit = {
+    assert(records.nonEmpty)
+    assertEquals(records.size, size)
+    assert(!records.exists(_.address.town.contains(", COUNTY")))
+    assert(!records.exists(_.address.town.contains(", CO. ")))
+    assert(!records.exists(_.address.town.contains(", CO ")))
+    assert(!records.exists(r => r.address.town == r.address.county.toString.toUpperCase()))
+    assert(records.exists(_.address.line2.nonEmpty))
+    assert(records.exists(_.address.line3.nonEmpty))
+  }
+}
